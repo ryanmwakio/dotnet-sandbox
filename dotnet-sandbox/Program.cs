394b81cced3805baf33761b22cffa9ae248af7b2
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-17 19:30:54 +0300

    initial commit

---
+++ dotnet-sandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        foreach (string drive in Environment.GetLogicalDrives())
+            Console.WriteLine("Drive: {0}", drive);
+
+        //get system details
+        Console.WriteLine("OS: {0}", Environment.OSVersion);
+        Console.WriteLine("Number of processors: {0}",
+
+        Environment.ProcessorCount);
+        Console.WriteLine(".NET Version: {0}",
+        Environment.Version);
+    }
+}
