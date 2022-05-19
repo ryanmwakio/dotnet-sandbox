0b4656764a67c77a6aae96f909023b4ef46215e2
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-19 12:10:36 +0300

    parsing types

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -9,6 +9,33 @@ public class Program
         bool b = new bool();
         double d = new double();
         DateTime dt = new DateTime();
-        Console.WriteLine(dt);
+
+        double myEps = double.Epsilon;
+
+        //members of the System.Char class
+        char myChar = 'a';
+        Console.WriteLine(char.IsDigit(myChar));
+        Console.WriteLine(char.IsWhiteSpace("Hello There", 5));
+
+        ParseFromStrings();
+
+    }
+
+    // Parsing types
+    static void ParseFromStrings()
+    {
+        Console.WriteLine("=> Data type parsing:");
+        bool b = bool.Parse("True");
+
+        Console.WriteLine("Value of b: {0}", b);
+        double d = double.Parse("99.884");
+        Console.WriteLine("Value of d: {0}", d);
+
+        int i = int.Parse("8");
+        Console.WriteLine("Value of i: {0}", i);
+
+        char c = Char.Parse("w");
+        Console.WriteLine("Value of c: {0}", c);
+
     }
 }
